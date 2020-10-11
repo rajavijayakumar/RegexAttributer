2bec2cc4568f17a3618f8a529cb83e2723d77b0c
Raja Nayagam <zohari.raja@gmail.com>
2020-10-11 11:42:51 +0530

    Initial Commit

---
+++ RegexAttributer/RegexAttributer.h
@@ -0,0 +1,18 @@
+//
+//  RegexAttributer.h
+//  RegexAttributer
+//
+//  Created by raja vijaya kumar on 11/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegexAttributer.
+FOUNDATION_EXPORT double RegexAttributerVersionNumber;
+
+//! Project version string for RegexAttributer.
+FOUNDATION_EXPORT const unsigned char RegexAttributerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexAttributer/PublicHeader.h>
+
+
